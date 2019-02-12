db14f4c522cf7ab24579d22d4c788a8e59693fb2
spell1612 <spell1612@gmail.com>
2019-02-12 15:28:09 +0530

    finished user registration

--- smack/Controller/CreateVC.swift
+++ smack/Controller/CreateVC.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class CreateVC: UIViewController {
-
+    
+    @IBOutlet weak var usernameField: TextFieldAttributes!
+    @IBOutlet weak var passwordField: TextFieldAttributes!
+    @IBOutlet weak var emailField: TextFieldAttributes!
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,5 +25,21 @@ class CreateVC: UIViewController {
        performSegue(withIdentifier: "unwindToChannelVC", sender: nil)
     }
     
-
+    @IBAction func avatarPick(_ sender: Any) {
+    }
+    
+    @IBAction func createAccount(_ sender: Any) {
+        guard let email=emailField.text, emailField.text != "" else {return}
+        guard let pass=passwordField.text, passwordField.text != "" else {return}
+        
+        AuthService.selfInstance.resgisterUser(email: email, password: pass) { (success) in
+            if success {print("Registered User")}
+            //here, success in passed to the target func and in the func a call for this definition is done in the parametres, so the var after grabbing stuff there is passed back. Hence we can check if the reg was a success or not
+            
+        }
+        
+    }
+    
+    @IBAction func bgColorPick(_ sender: Any) {
+    }
 }
